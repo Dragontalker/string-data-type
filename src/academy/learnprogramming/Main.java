6e1620011e1dd2917c185ba7107f86dc2137951b
Richard <richard.yang.tong@gmail.com>
2021-04-29 09:17:28 -0400

    Added some recaps on previous covered primitive types.

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -3,6 +3,13 @@ package academy.learnprogramming;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // byte
+        // short
+        // int
+        // long
+        // float
+        // double
+        // char
+        // boolean
     }
 }
